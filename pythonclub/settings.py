a567b9e0dcc27967057df1e22cc06d3608d0456c
Jiae Lee <jiaelee1010@gmail.com>
2021-04-19 16:02:59 -0700

    remove secret_key

--- pythonclub/settings.py
+++ pythonclub/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y@t(2zipqswj1pb^xk*!diqb&38o1linpf7xejni(j@7op!cmy'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
