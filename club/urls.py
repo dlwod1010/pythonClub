3095ada890ea347413512f34746995eb5ce6c679
Jiae Lee <jiaelee1010@gmail.com>
2021-04-26 16:08:34 -0700

    Create the first view

--- club/urls.py
+++ club/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
+# Only the index file can use empty quotes for the initial path statement.
 urlpatterns = [
-   
+    path('', views.index, name='index'),
 ]
