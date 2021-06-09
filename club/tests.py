2625fdf4ed46be3cf3e9d60773065a5376464190
Jiae Lee <jiaelee1010@gmail.com>
2021-06-08 19:49:56 -0700

    More tests -Authentification and view

--- club/tests.py
+++ club/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from .models import meeting, meetingMinutes, resource, event
+from django.urls import reverse, reverse_lazy
+from .forms import meetingForm
+from django.contrib.auth.models import User
+from .views import index, getmeetings
 # Create your tests here.
 
 
@@ -38,11 +42,47 @@ class eventTest(TestCase):
     def test_table(self):
         self.assertEqual(str(event._meta.db_table), 'event')
 
+
 class meetingFormTest(TestCase):
     def meetingFormIsValid(self):
-        form = meetingForm(data ={
-            'meetingTitle' : "title",
-            'meetingDate' : "01/01/2021",
-            'meetingLocationAt' : "JJ building",
-            'meetingAgenda' : "agenda"})
+        form = meetingForm(data={
+            'meetingTitle': "title",
+            'meetingDate': "01/01/2021",
+            'meetingLocationAt': "JJ building",
+            'meetingAgenda': "agenda"})
         self.assertTrue(form.is_valid())
+
+
+class Authentification_Test(TestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(
+            username='testuser1', password='P@ssw0rd1')
+        self.meeting = meeting.objects.create(
+            meetingTitle='Javascript',
+            meetingDate='2021-01-02',
+            meetingLocationAt='JJ building',
+            meetingAgenda='agenda')
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('newmeeting'))
+        self.assertRedirects(
+            response, '/accounts/login/?next=/club/newmeeting/')
+
+    def test_Logged_in_uses_correct_template(self):
+        login = self.client.login(username='testuser1', password='P@ssw0rd1')
+        response = self.client.get(reverse('newmeeting'))
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'club/newmeeting.html')
+
+
+class IndexTest(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+
+class GetMeetingsTest(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('meeting'))
+        self.assertEqual(response.status_code, 200)
